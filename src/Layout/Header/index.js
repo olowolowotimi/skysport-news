a97ec5c69ede5a35f6fa0111e1657f392dd0c938
olowolowotimi <62831854+olowolowotimi@users.noreply.github.com>
2020-12-11 09:48:57 +0100

    initial commit

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,109 +1,41 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { Menu, Search } from '@material-ui/icons';
-import Container from '@material-ui/core/Container'
-import { RiUserFill } from 'react-icons/ri';
+import topStyle from './style/Header.module.css';
+import {BsPersonFill} from 'react-icons/bs';
+import {BiSearch} from 'react-icons/bi'
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: "1",
-  },
- header: {
-      border: "1px solid red",
-      backgroundImage: "linear-gradient(0deg,rgba(0,0,0,.1),transparent 50%),linear-gradient(90deg,#002672 0,#002672 53px,#e10000 0,#e10000)", 
+const Header = () => {
+  return(
+    <div>
+    <div id={topStyle.body}>
+      <div className={topStyle.blue}>
+       <img src="/static/img/skysporttrans.svg" className={topStyle.blueImg} alt="transparent logo"/>
+      </div>
+      <div className={topStyle.red}>
+        <div className={topStyle.content}>
+        <p>Get Sky Sport</p>
+        <p><BsPersonFill /> Login</p>
+        <input type="text" placeholder="Search Sky Sport" />
+        <span><BiSearch/></span>
+        </div>
+      </div>
+      </div>
+      <div className={topStyle.subHead}>
+        <ul>
+        <li>Home</li>
+        <li>Sport</li>
+        <li>Scores</li>
+        <li>Videos</li>
+        <li>TV</li>
+        <li>Sky Bet</li>
+        <li>Games</li>
+        <li>More</li>
+        </ul>
+        <button>Watch Sky Sports</button>
+      </div>
       
 
-
- },
-  
-  title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  img: {
-    height: "30px",
-    width: "95px",
-    // display: "flex",
-    // position: "relative",
-    // zIndex: "2",
-    // justifyContent: "flex-start",
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
-export default function SearchAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.header}>
-        <Container maxWidth="md">
-
-                <Toolbar>
-          
-          <img src="/static/img/skysporttrans.svg" alt="" className={classes.img}/>
-          
-          
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <Search />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
-        </Toolbar>
-        </Container>
     </div>
-  );
+  )
 }
+export default Header
--- src/container/Newsfeed/index.js
+++ src/container/Newsfeed/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../../../components/Card';
+import NewsCard from '../../../components/Card';
 import { Grid } from "@material-ui/core"; 
 import feedStyle from './style/Newsfeed.module.css';
 
@@ -7,48 +7,117 @@ import feedStyle from './style/Newsfeed.module.css';
 const Newsfeed = () => {
     return(
         <div>
-            <div className={feedStyle.biggerCard}>
-            <Grid item xs={4} lg={12}>
-            <Card />
-        </Grid>
+            
+             <Grid item xs={12} lg={4}>
+                       
+                    </Grid>
+                    
+            <section id={feedStyle.newscontainer}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                            <div className={feedStyle.imageprop}>
+                                <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                            
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                        <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                        <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                        <img src="/static/img/golf.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Golf</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                        <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                        <img src="/static/img/skysportimg.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        
+                        </NewsCard>
+                    </Grid>
+                </Grid>
+            </section>
+            <Grid>
+               
+                    <h2 className={feedStyle.text}>Live On Sky</h2>
+                    <div  className={feedStyle.sectiontwo}>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                                <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        </NewsCard>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <NewsCard>
+                        <div className={feedStyle.imageprop}>
+                                <img src="/static/img/boxing.jpg" alt="card image"/>
+                                </div>
+                                <div className={feedStyle.content}>
+                                    <p>Football</p>
+                                    <h3>Kelleher starts again, TAA captains liverpool LIVE!</h3>
+                                    </div>
+                        </NewsCard>
+                    </Grid>
+                </div>
+            </Grid>
         </div>
-        <section id={feedStyle.newscontainer}>
-        <Grid container spacing={2}>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        </Grid>
-        </section>
-        <Grid container spacing={2}>
-            <div  className={feedStyle.sectiontwo}>
-            <h2>Live On Sky</h2>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        <Grid item xs={4}>
-            <Card />
-        </Grid>
-        </div>
-        </Grid>
-
-        </div>
-     
-        )
+    )
 }
 
 export default Newsfeed
